718980a60695e5eb7fcb7cbeb6ce1abf0e912f32
Abhishek urs C J <abhishekurst@gmail.com>
2023-12-27 16:21:59 +0530

    Added getSdk version handler

---
+++ src/pubsub/handlers/GetSDKVersionHandler.js
@@ -0,0 +1,16 @@
+import BaseHandler from './BaseHandler';
+
+require('dotenv').config();
+
+export default class GetSDKVersionHandler extends BaseHandler {
+  constructor(handlerName) {
+    super(handlerName);
+  }
+
+  async handle(message) {
+    const file = require('../../../package.json');
+    const validationReport = file.version;
+    const reportstring = JSON.stringify({ report: validationReport });
+    return JSON.stringify({ report: validationReport });
+  }
+}

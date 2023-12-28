55ae65548f2b99d9c89b72994a5a9d5275b72cb4
Abhishek urs C J <abhishekurst@gmail.com>
2023-12-28 11:00:05 +0530

    Added getSDKVersion handler

--- src/IntentReader.js
+++ src/IntentReader.js
@@ -44,6 +44,7 @@ import LifecycleRecordHandler from './pubsub/handlers/lifecycleRecordHandler';
 import RegisterProviderHandler from './pubsub/handlers/RegisterProviderHandler';
 import GetEventResponse from './pubsub/handlers/GetEventResponse';
 import GetMethodResponseHandler from './pubsub/handlers/GetMethodResponseHandler';
+import GetSDKVersionHandler from './pubsub/handlers/GetSDKVersionHandler';
 
 const logger = require('./utils/Logger')('IntentReader.js');
 
@@ -61,6 +62,7 @@ const handlers = {
   stopLifecycleRecording: new LifecycleRecordHandler('stopLifecycleRecording'),
   getEventResponse: new GetEventResponse('getEventResponse'),
   getMethodResponse: new GetMethodResponseHandler('getMethodResponse'),
+  getSDKVersionHandler: new GetSDKVersionHandler('getSDKVersionHandler'),
   [CONSTANTS.CALL_METHOD]: new CallMethodHandler(CONSTANTS.CALL_METHOD),
   [CONSTANTS.HEALTH_CHECK]: new HealthCheckHandler(CONSTANTS.HEALTH_CHECK),
 };

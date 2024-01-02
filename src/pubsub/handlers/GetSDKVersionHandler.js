61473fc5e7618938c8f382d1886f5ac1ddc0bf1e
Jayaramaraju, Abhishek <Abhishek_Jayaramaraju@comcast.com>
2024-01-02 10:30:52 +0530

    Added minor change

--- src/pubsub/handlers/GetSDKVersionHandler.js
+++ src/pubsub/handlers/GetSDKVersionHandler.js
@@ -10,7 +10,6 @@ export default class GetSDKVersionHandler extends BaseHandler {
   async handle(message) {
     const file = require('../../../package.json');
     const validationReport = file.version;
-    const reportstring = JSON.stringify({ report: validationReport });
-    return JSON.stringify({ report: reportstring });
+    return JSON.stringify({ report: validationReport });
   }
 }

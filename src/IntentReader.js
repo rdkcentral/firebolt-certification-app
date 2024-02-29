17310d48443ec7ab42837ed4235e7ef23f821279
bp-vstaty366_comcast <bp-vstaty366@cable.comcast.com>
2024-02-29 11:46:06 +0200

    Revert "resolve conflicts"
    This reverts commit 62f309edf013bc6f6abebe0a117a0dcbf82fcc95.

--- src/IntentReader.js
+++ src/IntentReader.js
@@ -78,11 +78,6 @@ export default class IntentReader {
       return;
     }
 
-    if (message.metadata && message.metadata.target === 'MFOS') {
-      process.env.MF_VALUE = true;
-      process.env.PLATFORM = CONSTANTS.PLATFORM_MOCKOS;
-    }
-
     // Check for standalone and reportingId in the message and set them in process.env
     if ('standalone' in message) {
       process.env.STANDALONE = message.standalone;
@@ -117,8 +112,7 @@ export default class IntentReader {
       // Invoke toast notification for failed handle
       eventEmitter.emit('showToast', CONSTANTS.INTENT_ERR, CONSTANTS.TOAST_STATE_COMPL, CONSTANTS.TOAST_REF_COMPL, CONSTANTS.ERR_COLOR);
     }
-    
-    console.log('Response String: ' + JSON.stringify(responseString));
+
     return responseString;
   }
 }

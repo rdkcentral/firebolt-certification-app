dddfd787ce062304ba46660f3ce9eb23d1302dd9
Kummithi Guru Eswar Sainath Reddy <kummithi.sai00@gmail.com>
2024-04-03 20:49:42 +0530

    Correcting error messages

--- src/constant.js
+++ src/constant.js
@@ -134,8 +134,8 @@ export const CONSTANTS = {
   LIFECYCLE_RECORDING_STARTED: 'Lifecycle History Recording has been started for ',
   APPID_DOESNOT_MATCH: ' passed does not match launched app ',
   INVALID_LIFECYCLE_RECORD: 'Invalid lifecycle record request',
-  WRONG_ERROR_MESSAGE_FORMAT: 'Error Message Format Not Conforming to standard',
-  WRONG_RESPONSE_MESSAGE_FORMAT: 'Unexpected error encountered instead of the anticipated response',
+  WRONG_ERROR_MESSAGE_FORMAT: 'Error message conforming to not supported, but not in the standard format',
+  WRONG_RESPONSE_MESSAGE_FORMAT: 'Unexpected error encountered instead of the response',
   EXCLUDED_METHODS_FOR_SDK: [],
   EXCLUDED_METHODS_FOR_TRANSPORT: [],
   defaultSDKs: [

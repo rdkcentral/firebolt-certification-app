a8832eab93e05767919b9752058533ed026b7283
Keaton Sentak <ksentak01@gmail.com>
2024-05-17 14:11:44 -0400

    fix github pages

--- vueApp/vite.config.js
+++ vueApp/vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/firebolt-certification-app/' : '/'
 })

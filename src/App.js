233b3095dce3a29da2cf3be9c19700d5a4910838
Kummithi Guru Eswar Sainath Reddy <kummithi.sai00@gmail.com>
2024-11-19 15:30:12 +0530

    Reverted default mac code

--- src/App.js
+++ src/App.js
@@ -420,6 +420,11 @@ export default class App extends Base {
           if (query.params.pubSubToken) {
             process.env.PUB_SUB_TOKEN = query.params.pubSubToken;
           }
+
+          // Set the region if present
+          if (query.params.region) {
+            process.env.REGION = query.params.region;
+          }
           
           if (query.task) {
             setTimeout(() => {

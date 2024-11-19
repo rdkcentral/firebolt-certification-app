0590ce2a5fbe586c7c72e39c91c376dd5fdba145
Kummithi Guru Eswar Sainath Reddy <kummithi.sai00@gmail.com>
2024-11-19 17:03:58 +0530

    Fixed the RunTestHandler setup issue

--- webpack.prod.js
+++ webpack.prod.js
@@ -91,6 +91,10 @@ module.exports = {
             name: 'CensorData',
             alias: ['/plugins/censorData.json', '/src/source/censorData.json'],
           },
+          {
+            name: 'RunTestHandler',
+            alias: ['/plugins/runTestHandler.js', '/src/pubsub/handlers/RunTestHandler.js'],
+          },
         ],
         'resolve'
       ),

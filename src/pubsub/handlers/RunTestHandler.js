0e42bd94208bd48113ffc396689ff6d43b25b7d3
Nandana-NNR <nandana.n.raj97@gmail.com>
2025-01-24 15:18:35 +0530

    test fix

--- src/pubsub/handlers/RunTestHandler.js
+++ src/pubsub/handlers/RunTestHandler.js
@@ -42,7 +42,9 @@ export default class RunTestHandler extends BaseHandler {
       process.env.SLA_VALIDATION = false;
     }
     // Set the GLOBAL_SLA environment variable to the globalSLA parameter from the message, or null if it doesn't exist
-    process.env.GLOBAL_SLA = message.params.globalSLA || null;
+    if (message.params && message.params.globalSLA) {
+      process.env.GLOBAL_SLA = message.params.globalSLA || null;
+    }
 
     // Disable PENDING state in suite report via intent override [FIRECERT-838]
     if (message.params && message.params.methodsToBeExcluded && (message.params.certification != null || message.params.certification != undefined)) {

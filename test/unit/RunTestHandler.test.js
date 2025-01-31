41df2f9d06769c2735ce008c82831d6512c50fe2
bpvstaty366 <altimopsusvs@gmail.com>
2025-01-31 10:29:28 +0200

    add new files

--- test/unit/RunTestHandler.test.js
+++ test/unit/RunTestHandler.test.js
@@ -167,17 +167,17 @@ describe('RunTestHandler', () => {
       expect(process.env.FIREBOLT_SDK_VERSION).toBe(message.metadata.fireboltVersion);
       expect(process.env.TARGET_VERSION).toBe(message.metadata.targetVersion);
       expect(process.env.MODE).toBe(message.context.communicationMode);
-      expect(process.env.TARGET_PLATFORM).toBe('undefined');
+      expect(process.env.TARGET_PLATFORM).toBe(undefined);
     });
 
     /* test('should log error if there is an exception', () => {
              Object.defineProperty(process.env, "TARGET", { value: "test-target", writable: false });
              let responseString = runTestHandler.populateReportTitleValues(message);
              console.log(expect.getState().currentTestName + " : " + responseString)
- 
-             
+
+
              expect(logger).toHaveBeenCalled()
- 
+
          });*/
   });
 

bf509183e39823e136fc7cb969753a42c4032be1
Nandana-NNR <nandana.n.raj97@gmail.com>
2025-05-26 12:18:57 +0530

    new sdk transport invocation changes

--- src/FireboltTransportInvoker.js
+++ src/FireboltTransportInvoker.js
@@ -20,10 +20,11 @@ import Transport from '@firebolt-js/sdk/dist/lib/Transport';
 import { CONSTANTS } from './constant';
 const logger = require('./utils/Logger')('FireboltTransportInvoker.js');
 
-let invokeManager, invokeProvider;
+let invokeManager, invokeProvider, invokeSDK;
 try {
   invokeManager = require('../plugins/FireboltExtensionInvoker').default.invokeManager;
   invokeProvider = require('../plugins/FireboltExtensionInvoker').default.invokeProvider;
+  invokeSDK = require('../plugins/FireboltExtensionInvoker').default.invokeSDK;
 } catch (err) {
   logger.error(`Unable to import additional invoker - ${err.message}`);
 }
@@ -56,7 +57,10 @@ export default class FireboltTransportInvoker {
         return await invokeProvider.send(module, method, jsonParams);
       } else if (invoker == CONSTANTS.INVOKEMANAGER) {
         return await invokeManager.send(module, method, jsonParams);
-      } else {
+      } else if (invoker == CONSTANTS.INVOKESDK) {
+        return await invokeSDK.send(module, method, jsonParams);
+      }
+      else {
         return await Transport.send(module, method, jsonParams);
       }
     } else {

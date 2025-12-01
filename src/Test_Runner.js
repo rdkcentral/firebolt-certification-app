889a03c98ac5db342d0673565f8116e30950497c
Abhishek urs C J <abhishekurst@gmail.com>
2025-12-01 19:37:21 +0530

    fixed lint error

--- src/Test_Runner.js
+++ src/Test_Runner.js
@@ -771,7 +771,7 @@ export class Test_Runner {
   createUUID() {
     let dt = new Date().getTime();
     const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      const r = (dt + Math.random() * 16) % 16 | 0;
+      const r = ((dt + Math.random() * 16) % 16) | 0;
       dt = Math.floor(dt / 16);
       return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
     });
